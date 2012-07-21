3c4f866b3a60dd52ff4285100309494b5e97e92b
Guilherme de Paula Sant'Anna <guilhermepsantanna@hotmail.com>
2012-07-21 14:52:31 -0300

    Hello calculator

---
+++ app/app_delegate.rb
@@ -0,0 +1,8 @@
+class AppDelegate
+  def application(application, didFinishLaunchingWithOptions:launchOptions)
+  	alert = UIAlertView.new
+  	alert.message = "Hello calculator!"
+  	alert.show
+    true
+  end
+end
